27d93ffb3f979ee2c03db67992923d558893272a
Navya Govind <gnavya.2712@gmail.com>
2022-07-02 16:34:51 +0200

    ENH: added documentation

--- index.js
+++ index.js
@@ -3,18 +3,10 @@ import dotenv from 'dotenv'
 import request from 'request'
 dotenv.config()
 
-const client = new DiscordJS.Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
-    ]
-});
-
-client.on('ready', () => {
-    console.log('The bot is ready')
-})
-
-// break into two methods
+/**
+ * Replies to the provided message with a fact obtained by an API call
+ * @param {*} message The discord message to reply to
+ */
 function replyWithFact(message) {
     request.get({
         url: `https://api.api-ninjas.com/v1/facts?limit=1`,
@@ -22,17 +14,40 @@ function replyWithFact(message) {
             "X-Api-Key": process.env.API_KEY
         }
     }, (error, response, body) => {
-        const profile = JSON.parse(body)
-        message.reply({
-            content: profile[0].fact
-        })
+        const response_code = response.statusCode
+        if (response_code != 200) {
+            const error_msg = "An error occured when retrieving the fact. Error code - " + response_code
+            message.reply({
+                content: error_msg
+            })
+        } else {
+            const fact = JSON.parse(body)[0].fact
+            message.reply({
+                content: fact
+            })
+        }
     })
 }
 
+// give permissions to the bot
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+// informs when the bot is ready
+client.on('ready', () => {
+    console.log('The bot is ready')
+})
+
+// listens for '!fact' and replies to it with a fact
 client.on('messageCreate', (message) => {
     if (message.content === '!fact') {
         replyWithFact(message)
     }
 })
 
+// authenticate using the bot token
 client.login(process.env.TOKEN)
